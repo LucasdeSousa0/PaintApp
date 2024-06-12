1b403010baee8b84c5eeb993e2c52dd93ec1604e
LucasdeSousa0 <lucasdesousa9@gmail.com>
2024-06-12 15:23:13 -0300

    correção do código mandado anteriormente

--- main.py
+++ main.py
@@ -12,24 +12,35 @@ class PaintApp:
         self.stroke_size.set(1)
         self.stroke_color = StringVar()
         self.stroke_color.set("black")
+        
+        self.previousColor = StringVar()
+        self.previousColor.set("white")
+        
+        self.previousColor2 = StringVar()
+        self.previousColor2.set("white")
 
         self.textValue = StringVar()
 
         self.prevPoint = [0, 0]
         self.currentPoint = [0, 0]
 
+        frame1 = Frame(self.root, height=100, width=1100)
+        frame1.grid(row=0, column=0, sticky=NW)
+        
+        Toolbar(frame1, self)
+        SizeSelector(frame1, self)
+        ColorPicker(frame1, self)
+        ColorButtons(frame1, self)
+        SaveAndClear(frame1, self)
+        HelpMenu(frame1, self)
+        TextEntry(frame1, self)
+        
         self.canvas = Canvas(self.root, height=500, width=1100, bg="white")
         self.canvas.grid(row=1, column=0)
         self.canvas.bind("<B1-Motion>", self.paint)
         self.canvas.bind("<ButtonRelease-1>", self.paint)
         self.canvas.bind("<B3-Motion>", self.paint_right)
         self.canvas.bind("<Button-2>", self.write_text)
-
-        Toolbar(self)
-        ColorPicker(self)
-        SizeSelector(self)
-        SaveAndClear(self)
-        HelpMenu(self)
 
         self.root.resizable(False, False)
 
@@ -50,6 +61,11 @@ class PaintApp:
             self.stroke_color.set("black")
         else:
             self.stroke_color.set(selected_color[1])
+            self.previousColor2.set(self.previousColor.get())
+            self.previousColor.set(selected_color[1])
+
+            self.previousColorButton["bg"] = self.previousColor.get()
+            self.previousColor2Button["bg"] = self.previousColor2.get()
 
     def paint(self, event):
         x, y = event.x, event.y
@@ -102,67 +118,71 @@ class PaintApp:
         self.canvas.create_text(event.x, event.y, text=self.textValue.get())
 
 class Toolbar:
-    def __init__(self, app):
-        self.frame = Frame(app.root, height=100, width=1100)
-        self.frame.grid(row=0, column=0, sticky=NW)
-
-        self.create_tools(app)
-
-    def create_tools(self, app):
-        tools_frame = Frame(self.frame, height=100, width=100, relief=SUNKEN, borderwidth=3)
+    def __init__(self, parent, app):
+        tools_frame = Frame(parent, height=100, width=100, relief=SUNKEN, borderwidth=3)
         tools_frame.grid(row=0, column=0)
 
         pencil_button = Button(tools_frame, text="Pencil", width=10, command=app.use_pencil)
         pencil_button.grid(row=0, column=0)
         eraser_button = Button(tools_frame, text="Eraser", width=10, command=app.use_eraser)
         eraser_button.grid(row=1, column=0)
-        Label(tools_frame, text="Tools", width=10).grid(row=3, column=0)
+        Label(tools_frame, text="Tools", width=10).grid(row=2, column=0)
+
+class SizeSelector:
+    def __init__(self, parent, app):
+        size_frame = Frame(parent, height=100, width=100, relief=SUNKEN, borderwidth=3)
+        size_frame.grid(row=0, column=1)
+
+        Button(size_frame, text="Default", width=10, command=app.use_pencil).grid(row=0, column=0)
+        OptionMenu(size_frame, app.stroke_size, *[1, 2, 3, 4, 5, 10]).grid(row=1, column=0)
+        Label(size_frame, text="Size", width=10).grid(row=2, column=0)
 
 class ColorPicker:
-    def __init__(self, app):
-        self.frame = Frame(app.root, height=100, width=100, relief=SUNKEN, borderwidth=3)
-        self.frame.grid(row=0, column=2)
+    def __init__(self, parent, app):
+        color_box_frame = Frame(parent, height=100, width=100, relief=SUNKEN, borderwidth=3)
+        color_box_frame.grid(row=0, column=2)
 
-        self.create_color_picker(app)
+        Button(color_box_frame, text="Select Color", width=10, command=app.select_color).grid(row=0, column=0)
+        app.previousColorButton = Button(color_box_frame, text="Previous", width=10, command=lambda: app.stroke_color.set(app.previousColor.get()))
+        app.previousColorButton.grid(row=1, column=0)
+        app.previousColor2Button = Button(color_box_frame, text="Previous2", width=10, command=lambda: app.stroke_color.set(app.previousColor2.get()))
+        app.previousColor2Button.grid(row=2, column=0)
 
-    def create_color_picker(self, app):
-        Button(self.frame, text="Select Color", width=10, command=app.select_color).grid(row=0, column=0)
-        
-class SizeSelector:
-    def __init__(self, app):
-        self.frame = Frame(app.root, height=100, width=100, relief=SUNKEN, borderwidth=3)
-        self.frame.grid(row=0, column=1)
+class ColorButtons:
+    def __init__(self, parent, app):
+        colors_frame = Frame(parent, height=100, width=200, relief=SUNKEN, borderwidth=3)
+        colors_frame.grid(row=0, column=3)
 
-        self.create_size_selector(app)
-
-    def create_size_selector(self, app):
-        Button(self.frame, text="Default", width=10, command=app.use_pencil).grid(row=0, column=0)
-        OptionMenu(self.frame, app.stroke_size, *[1, 2, 3, 4, 5, 10]).grid(row=1, column=0)
-        Label(self.frame, text="Size", width=10).grid(row=2, column=0)
+        colors = ["Red", "Green", "Blue", "Yellow", "Orange", "Purple"]
+        for i, color in enumerate(colors):
+            Button(colors_frame, text=color, bg=color.lower(), width=10, command=lambda c=color.lower(): app.stroke_color.set(c)).grid(row=i//2, column=i%2)
 
 class SaveAndClear:
-    def __init__(self, app):
-        self.frame = Frame(app.root, height=100, width=100, relief=SUNKEN, borderwidth=3)
-        self.frame.grid(row=0, column=4)
+    def __init__(self, parent, app):
+        save_image_frame = Frame(parent, height=100, width=100, relief=SUNKEN, borderwidth=3)
+        save_image_frame.grid(row=0, column=4)
 
-        self.create_save_and_clear(app)
-
-    def create_save_and_clear(self, app):
-        Button(self.frame, text="Save", width=10, command=app.save_image).grid(row=0, column=0)
-        Button(self.frame, text="New", width=10, command=app.create_new).grid(row=1, column=0)
-        Button(self.frame, text="Clear", width=10, command=app.clear).grid(row=2, column=0)
+        Button(save_image_frame, text="Save", width=10, command=app.save_image).grid(row=0, column=0)
+        Button(save_image_frame, text="New", width=10, command=app.create_new).grid(row=1, column=0)
+        Button(save_image_frame, text="Clear", width=10, command=app.clear).grid(row=2, column=0)
 
 class HelpMenu:
-    def __init__(self, app):
-        self.frame = Frame(app.root, height=100, width=100, relief=SUNKEN, borderwidth=3)
-        self.frame.grid(row=0, column=5)
+    def __init__(self, parent, app):
+        help_setting_frame = Frame(parent, height=100, width=100, relief=SUNKEN, borderwidth=3)
+        help_setting_frame.grid(row=0, column=5)
 
-        self.create_help_menu(app)
+        Button(help_setting_frame, text="Help", width=10, command=app.show_help).grid(row=0, column=0)
+        Button(help_setting_frame, text="Settings", width=10, command=app.show_settings).grid(row=1, column=0)
+        Button(help_setting_frame, text="About", width=10, command=app.show_about).grid(row=2, column=0)
 
-    def create_help_menu(self, app):
-        Button(self.frame, text="Help", width=10, command=app.show_help).grid(row=0, column=0)
-        Button(self.frame, text="Settings", width=10, command=app.show_settings).grid(row=1, column=0)
-        Button(self.frame, text="About", width=10, command=app.show_about).grid(row=2, column=0)
+class TextEntry:
+    def __init__(self, parent, app):
+        text_frame = Frame(parent, height=100, width=200, relief=SUNKEN, borderwidth=3)
+        text_frame.grid(row=0, column=6)
+
+        Label(text_frame, text="Write your Text here:", width=20).grid(row=0, column=0)
+        Entry(text_frame, textvariable=app.textValue, width=20).grid(row=1, column=0)
+        Button(text_frame, text="Clear", width=20, command=lambda: app.textValue.set("")).grid(row=2, column=0)
 
 if __name__ == "__main__":
     app = PaintApp()
